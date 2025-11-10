ee9a0a73c8bc54a21382c31119fd3011b21eb8b1
jeremingo <58335558+jeremingo@users.noreply.github.com>
2025-11-10 18:00:20 +0200

    gh-14 Convert keymap.json to keymap.c Using keymap from ortho_4x12 layout

---
+++ keyboards/boardsource/unicorne/keymaps/jeremingo/keymap.c
@@ -0,0 +1,168 @@
+#include QMK_KEYBOARD_H
+#include "keymap_dvorak.h"
+
+extern keymap_config_t keymap_config;
+
+#define RAISE_SPC LT(_RAISE, KC_SPC)
+#define LCTL_BSPC LCTL_T(KC_BSPC)
+#define LSFT_ENT LSFT_T(KC_ENT)
+
+enum tap_dances {
+    TD_PLY_MUTE,
+    TD_NXT_PRV
+};
+
+enum combos {
+    ENTLOW_CW
+};
+
+enum layers {
+  _QWERTY,
+  _DVORAK,
+  _LOWER,
+  _RAISE,
+  _ADJUST,
+  _NAV
+};
+
+enum keycodes {
+  LAYOUT = SAFE_RANGE
+};
+
+tap_dance_action_t tap_dance_actions[] = {
+  [TD_PLY_MUTE]  = ACTION_TAP_DANCE_DOUBLE(KC_MPLY, KC_MUTE),
+  [TD_NXT_PRV]  = ACTION_TAP_DANCE_DOUBLE(KC_MNXT, KC_MPRV)
+};
+
+const uint16_t PROGMEM entlow_combo[] = { LSFT_ENT, MO(_LOWER), COMBO_END };
+
+combo_t key_combos[] = {
+  [ENTLOW_CW]  = COMBO(entlow_combo, CW_TOGG)
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+[_QWERTY] = LAYOUT_split_3x6_3(
+  KC_TAB,         DV_QUOT, DV_COMM, DV_DOT,  DV_P,     DV_Y,       DV_F,      DV_G,      DV_C,    DV_R,    DV_L,  KC_BSPC,
+  LCTL_T(KC_ESC), DV_A,    DV_O,    DV_E,    DV_U,     DV_I,       DV_D,      DV_H,      DV_T,    DV_N,    DV_S,  DV_MINS,
+  KC_LSFT,        DV_SCLN, DV_Q,    DV_J,    DV_K,     DV_X,       DV_B,      DV_M,      DV_W,    DV_V,    DV_Z,  KC_ENT,
+                                    KC_LALT, LSFT_ENT, MO(_LOWER), LCTL_BSPC, RAISE_SPC, MO(_NAV)
+),
+
+[_DVORAK] = LAYOUT_split_3x6_3(
+  KC_TAB,         KC_QUOT, KC_COMM, KC_DOT,  KC_P,     KC_Y,       KC_F,      KC_G,      KC_C,    KC_R,    KC_L,  KC_BSPC,
+  LCTL_T(KC_ESC), KC_A,    KC_O,    KC_E,    KC_U,     KC_I,       KC_D,      KC_H,      KC_T,    KC_N,    KC_S,  KC_SLSH,
+  KC_LSFT,        KC_SCLN, KC_Q,    KC_J,    KC_K,     KC_X,       KC_B,      KC_M,      KC_W,    KC_V,    KC_Z,  KC_ENT,
+                                    KC_LALT, LSFT_ENT, MO(_LOWER), LCTL_BSPC, RAISE_SPC, MO(_NAV)
+),
+
+[_LOWER] = LAYOUT_split_3x6_3(
+  KC_TILD, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   DV_LCBR, DV_RCBR, DV_QUES, DV_PLUS, _______,
+  KC_DEL,  KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC, KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, _______,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_HOME, KC_END,  KC_PIPE, _______, _______,
+                             _______, _______, _______, _______, _______, _______
+),
+
+[_RAISE] = LAYOUT_split_3x6_3(
+  KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   DV_LBRC, DV_RBRC, DV_SLSH, DV_EQL,  _______,
+  KC_DEL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    _______,
+  _______, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PGUP, KC_PGDN, KC_BSLS, _______, _______,
+                             _______, _______, _______, _______, _______, _______
+),
+
+[_ADJUST] = LAYOUT_split_3x6_3(
+  LALT(LCTL(KC_DEL)), QK_RBT,  _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_DEL,
+  LSFT(LCTL(KC_ESC)), RM_TOGG, RM_NEXT, UG_HUEU, UG_HUED, _______, _______, LAYOUT,  _______, _______, _______, _______,
+  _______,            _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+                                        _______, _______, _______, _______, _______, _______
+),
+
+[_NAV] = LAYOUT_split_3x6_3(
+  _______, KC_PSCR,        KC_BRID, KC_BRIU, LALT(KC_F4),     _______, _______, _______, _______, _______, KC_RGHT, _______,
+  _______, TD(TD_NXT_PRV), KC_VOLD, KC_VOLU, TD(TD_PLY_MUTE), _______, _______, KC_LEFT, _______, _______, _______, _______,
+  _______, _______,        _______, KC_DOWN, KC_UP,           _______, _______, _______, _______, _______, _______, _______,
+                                    _______, _______,         _______, _______, _______, _______
+)
+
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case LAYOUT:
+      if (record->event.pressed) {
+        switch (eeconfig_read_default_layer()) {
+          case 1UL << _QWERTY:
+            set_single_persistent_default_layer(_DVORAK);
+            break;
+          case 1UL << _DVORAK:
+            set_single_persistent_default_layer(_QWERTY);
+            break;
+        }
+      }
+      return false;
+      break;
+  }
+  return true;
+}
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+  return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
+
+uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case RAISE_SPC:
+    case LSFT_ENT:
+      return 200;
+    default:
+      return TAPPING_TERM;
+  }
+}
+
+bool caps_word_press_user(uint16_t keycode) {
+  switch (keycode) {
+    case DV_A:
+    case DV_B:
+    case DV_C:
+    case DV_D:
+    case DV_E:
+    case DV_F:
+    case DV_G:
+    case DV_H:
+    case DV_I:
+    case DV_J:
+    case DV_K:
+    case DV_L:
+    case DV_M:
+    case DV_N:
+    case DV_O:
+    case DV_P:
+    case DV_Q:
+    case DV_R:
+    case DV_S:
+    case DV_T:
+    case DV_U:
+    case DV_V:
+    case DV_W:
+    case DV_X:
+    case DV_Y:
+    case DV_Z:
+    case DV_MINS:
+      add_weak_mods(MOD_BIT(KC_LSFT));
+      return true;
+
+    case KC_1 ... KC_0:
+    case KC_BSPC:
+    case KC_DEL:
+    case DV_UNDS:
+    case KC_LEFT:
+    case KC_DOWN:
+    case KC_UP:
+    case KC_RGHT:
+      return true;
+
+    default:
+      return false;
+  }
+}
+
